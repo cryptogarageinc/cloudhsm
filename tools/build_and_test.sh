5f75eb40e5b70fad1e03db369dc44632b2b0d026
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2022-06-14 13:26:33 +0900

    fix: remove cmake

--- tools/build_and_test.sh
+++ tools/build_and_test.sh
@@ -9,26 +9,12 @@ cp -rp cloudhsm cloudhsm-util
 ls
 
 cd cloudhsm-util
-rm -rf build
-mkdir build
-cd build
-cmake --version
-cmake .. -DENABLE_SHARED=on -DCMAKE_BUILD_TYPE=Release -DTARGET_RPATH="/usr/local/lib"
-make
-cp ./Release/libcloudhsmpkcs11util.* /github/workspace/dist/usr/local/lib
-ls /github/workspace/dist
-ls /github/workspace/dist/usr
-ls /github/workspace/dist/usr/local
-ls /github/workspace/dist/usr/local/lib
 
-cd /github/workspace/dist
-cp -rf usr /
-ls -l /usr/local/*
 ls -l /usr/local/go/bin
 
 env
 cd /github/workspace/cloudhsm-util
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib" /usr/local/go/bin/go mod download
+go mod download
 echo "---- go test start ----"
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib/pkcs11" /usr/local/go/bin/go test
 echo "---- go test end ----"
